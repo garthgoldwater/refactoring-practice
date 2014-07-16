5a65330f6b8fde79a2fe1573d0d66873b830fe64
Garth Goldwater <garth@Garths-MacBook-Pro.local>
2014-07-16 14:55:06 -0400

    clean up hotel_loader, take filename, extract print from initialize

--- hotel_database/hotel_loader.rb
+++ hotel_database/hotel_loader.rb
@@ -1,11 +1,19 @@
 require "csv"
 
 class HotelLoader
-  def initialize
-      CSV.foreach("hotels.csv", headers: true) do |hotel|
-        puts hotel["Hotel"].lstrip.chomp
-      end
+  def initialize(filename_of_hotel_csv="hotels.csv")
+    @hotels = {}
+    CSV.foreach(filename_of_hotel_csv, headers: true) do |hotel|
+      @hotels[hotel["Hotel"].strip] = hotel
+    end
+  end
+
+  def print_hotels
+    @hotels.keys.each do |hotel_name|
+      puts hotel_name
+    end
   end
 end
 
-HotelLoader.new
+hotel_loader = HotelLoader.new
+hotel_loader.print_hotels
